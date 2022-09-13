9bc5972176f37cc67bfa132c1df72027f673e661
ZMizgalski <zbigniew.mizgalskifullstack@gmail.com>
2022-09-13 17:51:51 +0200

    big update + refractor

--- src/main/java/commands/GetSwordCommand.java
+++ src/main/java/commands/GetSwordCommand.java
@@ -7,6 +7,9 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import swords.DragonSword;
+
+import java.util.ArrayList;
 
 public class GetSwordCommand implements CommandExecutor {
 
@@ -20,6 +23,7 @@ public class GetSwordCommand implements CommandExecutor {
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
 
         if (commandSender instanceof Player player) {
+            DragonSword dragonSword = new DragonSword(this.plugin, "elo", new ArrayList<>());
             player.sendMessage("Nice sword");
             ItemStack item = new ItemStack(Material.DIAMOND_SWORD);
             player.getInventory().addItem(item);
